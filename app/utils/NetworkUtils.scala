7b1e1ae9b26a10a2b52bfa7710c98f7f9a59ed4a
Adrien <adrien.crovetto@gmail.com>
2016-10-25 15:07:54 +0200

    more test

--- app/utils/NetworkUtils.scala
+++ app/utils/NetworkUtils.scala
@@ -1,11 +1,37 @@
 package utils
 
-import java.net.InetAddress
+import java.net.{NetworkInterface, InetAddress}
+
+import scala.collection.JavaConversions._
 
 object NetworkUtils {
 
+  def getIps(): List[String] = {
+    val rez = complexIp().map(_.getInterfaceAddresses.head.getAddress.getHostAddress()).map(_.toString)
+    basicIp() :: rez
+  }
+
   def getIp(): String = {
+    basicIp()
+  }
+
+  private def basicIp():String = {
     val thisIp: InetAddress = InetAddress.getLocalHost()
     thisIp.getHostAddress()
+  }
+
+  def complexIp():List[NetworkInterface] = {
+    val interfaces = NetworkInterface.getNetworkInterfaces().toList
+    interfaces.foreach{ i =>
+      println(
+        s"""
+           |name : ${i.getName}
+           |inet : ${i.getInetAddresses}
+           |adr  : ${i.getInterfaceAddresses}
+           |hard : ${i.getHardwareAddress}
+         """.stripMargin)
+    }
+
+    interfaces
   }
 }
